c07c888ab8ee53349b496e9269d507b7c6f6de46
Thomas Churchman <thomas@kepow.org>
2016-06-09 14:38:21 +0200

    Add a very basic experiment with a homeostatic agent.

--- enactiveagents/experiment/basic.py
+++ enactiveagents/experiment/basic.py
@@ -97,6 +97,100 @@ class BasicExperiment(experiment.Experiment):
     def get_world(self):
         return self.world
 
+class BasicHomeostaticExperiment(experiment.Experiment):
+    world_representation = [
+        "wwwwwwwwwwwwwww",
+        "w.............w",
+        "w.wwwwwww.....w",
+        "w.......wwwww.w",
+        "w.wwwww.......w",
+        "w.w.......w...w",
+        "w.w.wwwww.w...w",
+        "w.w.w...w.ww.ww",
+        "w.www.....w...w",
+        "w.....wwwww.h.w",
+        "wwwwwwwwwwwwwww"
+        ]
+
+    def __init__(self):
+        super(BasicHomeostaticExperiment, self).__init__()
+
+        # Parse world
+        self.world = self.parse_world(self.world_representation)
+
+        # Set up primitives
+        step = model.interaction.PrimitiveInteraction("Step")
+        turn_right = model.interaction.PrimitiveInteraction("Turn Right")
+        turn_left = model.interaction.PrimitiveInteraction("Turn Left")
+        feel = model.interaction.PrimitiveInteraction("Feel")
+        no_feel = model.interaction.PrimitiveInteraction("No Feel")
+        bump = model.interaction.PrimitiveInteraction("Bump")
+
+        # Define environment logic for primitives, these functions will be
+        # registered to the primitive interactions and will be called once
+        # the agent attempts to enact the primitive interaction. 
+        # The function can manipulate the world and the agents.
+        # The return value is the actual enacted interaction (i.e., can be 
+        # different form the attempted interaction).
+        def _step(world, agent, interaction):
+            if world.can_step(agent):
+                agent.step()
+                agent.add_to_homeostatic_value("energy", -0.1)
+                return step
+            else:
+                return bump
+
+        def _turn_right(world, agent, interaction):
+            agent.add_rotation(-90)
+            return turn_right
+        
+        def _turn_left(world, agent, interaction):
+            agent.add_rotation(90)
+            return turn_left
+
+        def _feel(world, agent, interaction):
+            if world.can_step(agent):
+                return no_feel
+            else:
+                return feel
+
+        # Register the previously defined functions.
+        enact_logic = {}
+        enact_logic[step] = _step
+        enact_logic[turn_right] = _turn_right
+        enact_logic[turn_left] = _turn_left
+        enact_logic[feel] = _feel
+
+        # Set primitives known/enactable by the agents.
+        primitives = []
+        primitives.append(step)
+        primitives.append(turn_right)
+        primitives.append(turn_left)
+        primitives.append(feel)
+        primitives.append(no_feel)
+        primitives.append(bump)
+
+        # Set intrinsic homeostatic motivation values.
+        motivation = {}
+        motivation[step] = lambda agent: agent.get_homeostatic_value("energy") * 0.1
+        motivation[turn_right] = lambda agent: -2
+        motivation[turn_left] = lambda agent: -2
+        motivation[feel] = lambda agent: 0
+        motivation[no_feel] = lambda agent: -1
+        motivation[bump] = lambda agent: -10
+
+        for entity in self.world.get_entities():
+            if isinstance(entity, model.agent.Agent):
+                self.world.add_enact_logic(entity, enact_logic)
+                entity.set_primitives(primitives)
+                entity.set_motivation(motivation)
+                if isinstance(entity, model.agent.HomeostaticConstructiveAgent):
+                    entity.set_homeostatic_value("energy", 100)
+
+
+    def get_world(self):
+        return self.world
+
 class BasicCoexsistenceExperiment(experiment.Experiment):
     world_representation = [
         "wwwww",
