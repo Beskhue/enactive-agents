0ff2cc8da7f5f0dbddfa826a65400927c8a79002
Thomas Churchman <thomas@kepow.org>
2016-10-21 02:17:17 +0200

    Fix wall init method.

--- enactiveagents/model/structure.py
+++ enactiveagents/model/structure.py
@@ -16,7 +16,7 @@ class Wall(Structure):
     """
     Class representing a wall structure.
     """
-    def __init__():
+    def __init__(self):
         super(Wall, self).__init__()
         self.height = 1
         self.width = 1
