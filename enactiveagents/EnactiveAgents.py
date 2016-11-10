2f4bfa8b7d6feb70bd2b76c9c2f3f9113f6f4312
Thomas Churchman <thomas@kepow.org>
2016-11-11 00:27:07 +0100

    Load collaborative destroy experiment

--- enactiveagents/EnactiveAgents.py
+++ enactiveagents/EnactiveAgents.py
@@ -75,7 +75,7 @@ def main():
     event_manager.register_listener(heart_beat)
 
     # Initialize and register the world.
-    experiment_ = experiment.basic.BasicVisionCoexsistencePushExperiment()
+    experiment_ = experiment.basic.BasicVisionCoexsistenceDestroyExperiment()
     world = experiment_.get_world()
     event_manager.register_listener(world)
     AppState.get_state().set_world(world)
