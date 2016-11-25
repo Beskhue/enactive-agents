80c9986b60814cd5be4899e3dbb78272063370d3
Thomas Churchman <thomas@kepow.org>
2016-11-26 00:34:33 +0100

    Add example of loading an experiment.

--- enactiveagents/EnactiveAgents.py
+++ enactiveagents/EnactiveAgents.py
@@ -85,6 +85,7 @@ def main():
     event_manager.register_listener(heart_beat)
 
     # Initialize and register the world.
+    #experiment_ = experiment.experiment.Experiment.load_experiment("20161126T003019.p")
     experiment_ = experiment.basic.BasicCoexsistenceExperiment()
     AppState.get_state().set_experiment(experiment_)
     world = experiment_.get_world()
