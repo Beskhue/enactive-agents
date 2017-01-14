0e5923eca1d368cae8f1cb3a691d52997372b600
Beskhue <thomas@kepow.org>
2017-01-14 03:17:07 +0100

    Simplify structure code.

--- enactiveagents/model/structure.py
+++ enactiveagents/model/structure.py
@@ -16,10 +16,7 @@ class Wall(Structure):
     """
     Class representing a wall structure.
     """
-    def __init__(self):
-        super(Wall, self).__init__()
-        self.height = 1
-        self.width = 1
+    pass
 
 class Block(Structure):
     """
